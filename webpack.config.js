e5fcdcc0b0d02ce2ee5628e484aff12f9e2bf977
Arne Hamann <git@arne.email>
2025-02-28 10:19:38 +0100

    Distributed via github pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://github.com/MTT-Solutions/MTT-Icon-Finder/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://github.com/MTT-Solutions/MTT-Icon-Finder/dist"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
